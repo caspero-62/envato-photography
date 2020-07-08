d8b99dc0012cb494ba4744f8b6a875fe28258fb0
Caspero-62 <nwaforuchechukwu2007@gmail.com>
2020-07-08 23:49:47 +0100

    added typewriter effect for header
    added animation for images

--- index.js
+++ index.js
@@ -11,3 +11,43 @@ const scrollTrigger = () => {
 }
 
 window.addEventListener('scroll', scrollTrigger);
+
+// set up text to print, each item in array is new line
+const aText =["We craft beautiful experiences that are worth sharing"];
+
+let iSpeed = 100; // time delay of print out
+let iIndex = 0; // start printing array at this posision
+let iArrLength = aText[0].length; // the length of the text array
+let iScrollAt = 20; // start scrolling up at this many lines
+     
+let iTextPos = 0; // initialise text position
+let sContents = ''; // initialise contents letiable
+let iRow; // initialise current row
+     
+function typewriter() {
+
+    sContents =  ' ';
+    iRow = Math.max(0, iIndex-iScrollAt);
+    let destination = document.getElementById("container__text");
+     
+    while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+    }
+
+    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+
+    if ( iTextPos++ == iArrLength ) {
+        iTextPos = 0;
+        iIndex++;
+        if ( iIndex != aText.length ) {
+            iArrLength = aText[iIndex].length;
+            setTimeout("typewriter()", 500);
+        }
+        } else {
+            setTimeout("typewriter()", iSpeed);
+        }
+    }
+    
+    
+typewriter();
+    
